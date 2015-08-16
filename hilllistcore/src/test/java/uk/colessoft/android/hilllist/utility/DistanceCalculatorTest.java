cbfa19347ca2f1c6f66637e1d8ec3a1baa028aa4
Adam Coles <colesadam@gmail.com>
2015-08-16 21:23:16 +0100

    added module with a test

---
+++ hilllistcore/src/test/java/uk/colessoft/android/hilllist/utility/DistanceCalculatorTest.java
@@ -0,0 +1,12 @@
+package uk.colessoft.android.hilllist.utility;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+public class DistanceCalculatorTest {
+
+    @Test
+    public void distanceCalcWorks(){
+        assertEquals(DistanceCalculator.CalculationByDistance(55.0, 60, 0, -1.5),563.11,0.1);
+    }
+}
