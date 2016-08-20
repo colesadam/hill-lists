7a382a496dc753fe03710c1f22f15906e8baa21c
Adam Coles <colesadam@gmail.com>
2016-08-20 23:52:40 +0100

    working with fixes

--- app/build/generated/source/buildConfig/debug/uk/colessoft/android/hilllist/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/uk/colessoft/android/hilllist/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "uk.colessoft.android.hilllist";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 30;
-  public static final String VERSION_NAME = "2.2";
+  public static final int VERSION_CODE = 34;
+  public static final String VERSION_NAME = "2.21";
 }
