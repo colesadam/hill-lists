d4a900dcef9c921994f20d8f0c50aeb32c1989f2
Adam Coles <colesadam@gmail.com>
2017-11-19 18:08:34 +0000

    removed FolderPicker

--- app/src/main/java/uk/colessoft/android/hilllist/activities/BaggingExportActivity.java
+++ app/src/main/java/uk/colessoft/android/hilllist/activities/BaggingExportActivity.java
@@ -1,47 +1,29 @@
 package uk.colessoft.android.hilllist.activities;
 
 import android.app.Activity;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.os.ParcelFileDescriptor;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.Reader;
 import java.io.Writer;
-import java.util.Date;
 
 import javax.inject.Inject;
 
 import uk.colessoft.android.hilllist.BHApplication;
 import uk.colessoft.android.hilllist.R;
-import uk.colessoft.android.hilllist.activities.dialogs.FolderPicker;
 import uk.colessoft.android.hilllist.database.DbHelper;
 
-public class BaggingExportActivity extends AppCompatActivity implements OnClickListener,
-        DialogInterface.OnClickListener {
-
-    private FolderPicker mFolderDialog;
-    private FolderPicker mFileDialog;
-    private View mPickFolder;
-    private View mPickFile;
+public class BaggingExportActivity extends AppCompatActivity {
 
     private static final int READ_REQUEST_CODE = 42;
     private static final int WRITE_REQUEST_CODE = 43;
@@ -49,47 +31,11 @@ public class BaggingExportActivity extends AppCompatActivity implements OnClickL
     @Inject
     DbHelper dbAdapter;
 
-    private String filePath;
-
-    private static String folderPath = Environment
-            .getExternalStorageDirectory().getAbsolutePath();
-
-    /**
-     * Called when the activity is first created.
-     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ((BHApplication) getApplication()).getDbComponent().inject(this);
         setContentView(R.layout.bagging_export);
-        mPickFolder = findViewById(R.id.pick_folder);
-        mPickFolder.setOnClickListener(this);
-        mPickFile = findViewById(R.id.pick_file);
-        View folderPathView = findViewById(R.id.folder_path);
-        View filePathView = findViewById(R.id.file_path);
-
-        mPickFile.setOnClickListener(this);
-
-    }
-
-    private void generateCsvFile(String sFileName) {
-
-        Cursor baggedCursor = dbAdapter.getBaggedHillList();
-        startManagingCursor(baggedCursor);
-        try {
-            File gpxfile = new File(folderPath, "bagging_export.csv");
-
-            FileWriter writer = new FileWriter(gpxfile);
-
-            writeCsvFromCursor(baggedCursor, writer);
-            Toast backedUp = Toast.makeText(getApplication(), "Backed up to " + gpxfile.getAbsolutePath(),
-                    Toast.LENGTH_SHORT);
-            backedUp.show();
-        } catch (IOException e) {
-            Toast failed = Toast.makeText(getApplication(), "Backup Failed", Toast.LENGTH_LONG);
-            failed.show();
-        }
-
     }
 
     private void writeCsvFile(FileOutputStream stream) {
@@ -114,7 +60,6 @@ public class BaggingExportActivity extends AppCompatActivity implements OnClickL
         if (baggedCursor.moveToFirst()) {
             // Iterate over each cursor.
             do {
-
                 writer.append("'");
                 writer.append(baggedCursor.getString(0));
                 writer.append("'");
@@ -131,28 +76,10 @@ public class BaggingExportActivity extends AppCompatActivity implements OnClickL
             } while (baggedCursor.moveToNext());
         }
 
-
         writer.flush();
         writer.close();
     }
 
-    public void onClick(DialogInterface dialog, int which) {
-        if (dialog == mFolderDialog) {
-            ((TextView) findViewById(R.id.folder_path)).setText(mFolderDialog
-                    .getPath());
-            folderPath = mFolderDialog.getPath();
-            generateCsvFile("bagging.csv");
-        }
-        if (dialog == mFileDialog) {
-            String path = mFileDialog.getPath();
-            if (path == null) {
-                path = "no file selected";
-            }
-            ((TextView) findViewById(R.id.file_path)).setText(path);
-            filePath = path;
-            importCsvFile();
-        }
-    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -191,35 +118,5 @@ public class BaggingExportActivity extends AppCompatActivity implements OnClickL
 
     }
 
-    public void onClick(View v) {
-        if (v == mPickFolder) {
 
-            Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
-
-            intent.addCategory(Intent.CATEGORY_OPENABLE);
-
-            intent.setType("*/*");
-            intent.putExtra(Intent.EXTRA_TITLE, "bagging_export.csv");
-            startActivityForResult(intent, WRITE_REQUEST_CODE);
-
-        } else if (v == mPickFile) {
-
-            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-            intent.addCategory(Intent.CATEGORY_OPENABLE);
-            intent.setType("*/*");
-            startActivityForResult(intent, READ_REQUEST_CODE);
-
-        }
-
-    }
-
-    private void importCsvFile() {
-
-        try {
-            dbAdapter.importBagging(new FileReader(filePath));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-    }
 }
