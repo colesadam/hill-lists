842fdf9cd6c86a2e1b549b4775685730132f1cac
colesadam <colesadam@gmail.com>
2019-06-23 17:17:11 +0100

    move to rawquery

--- app/src/main/java/uk/colessoft/android/hilllist/dao/HillDetailDao.kt
+++ app/src/main/java/uk/colessoft/android/hilllist/dao/HillDetailDao.kt
@@ -3,7 +3,9 @@ package uk.colessoft.android.hilllist.dao
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.RawQuery
 import androidx.room.Transaction
+import androidx.sqlite.db.SupportSQLiteQuery
 import uk.colessoft.android.hilllist.model.HillDetail
 
 
@@ -14,7 +16,11 @@ interface HillDetailDao {
     @Query("SELECT * FROM hills WHERE h_id = :hillId")
     fun getHillDetail(hillId: Long): LiveData<HillDetail>
 
-    @Query("""SELECT h_id, latitude, longitude,Metres,Feet,
+    @RawQuery
+    fun getHillsRaw(groupId: String?, orderBy: String? = "height desc"/*, countryClause: String?, moreFilters: String?, , filter: Int?*/): LiveData<List<HillDetail>>
+
+    fun getHills(groupId: String?, orderBy: String? = "height desc"): LiveData<List<HillDetail>>{
+        val query = new SupportSQLiteQuery("""SELECT h_id, latitude, longitude,Metres,Feet,
         name,notes,dateClimbed,b_id from hills
         JOIN typeslink ON typeslink.hill_id = h_id
         LEFT JOIN bagging ON b_id = h_id
@@ -26,7 +32,8 @@ interface HillDetailDao {
         CASE WHEN :orderBy="name asc" THEN LOWER(name) END ASC,
         CASE WHEN :orderBy="name desc" THEN LOWER(name) END DESC,
         CASE WHEN :orderBy="height asc" THEN Metres END ASC,
-        CASE WHEN :orderBy="height desc" THEN Metres END DESC""")
-    fun getHills(groupId: String?, orderBy: String? = "height desc"/*, countryClause: String?, moreFilters: String?, , filter: Int?*/): LiveData<List<HillDetail>>
+        CASE WHEN :orderBy="height desc" THEN Metres END DESC""",
+        new Object[]{userId})
+    }
 
 }
